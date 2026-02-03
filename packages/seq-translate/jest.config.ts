e2a5029c0236818ab0c38100efd1c55b1fa9de9a
mykyta.forofontov <mykyta.forofontov@hotmail.com>
2026-02-03 11:47:25 +0000

    feat: add lint and test workflows with configuration for Jest and Allure reporting

---
+++ packages/seq-translate/jest.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from 'jest';
+import { baseConfig } from '../../jest.config.base';
+
+const config: Config = {
+  ...baseConfig,
+  displayName: 'seq-translate',
+  rootDir: '.',
+  testMatch: ['<rootDir>/src/tests/**/*.test.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/index.ts',
+    '!<rootDir>/src/browser-index.ts',
+    '!<rootDir>/src/browser-bundle.ts',
+    '!<rootDir>/src/**/*.test.ts',
+    '!<rootDir>/src/**/*.spec.ts',
+    '!<rootDir>/src/tests/**',
+  ],
+  coverageDirectory: 'coverage',
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
+  },
+  reporters: [
+    'default',
+    [
+      'jest-html-reporter',
+      {
+        pageTitle: 'Bioseq-Translate Test Report',
+        outputPath: 'coverage/test-report.html',
+        includeFailureMsg: true,
+        includeConsoleLog: true,
+      },
+    ],
+  ],
+};
+
+export default config;
